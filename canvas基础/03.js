75ff9546e2071faa178e0e39eece9e599bb7301f
Ataotao <tzvuf@163.com>
2016-12-02 08:50:47 +0800

    canvas基础/03.js

--- canvas基础/03.js
+++ canvas基础/03.js
@@ -1,6 +1,3 @@
-var WINDOW_WIDTH = 1024;
-var WINDOW_HEIGHT = 768;
-var RADIUS = 8;
 var digit = [
     [
         [0, 0, 1, 1, 1, 0, 0],
@@ -135,24 +132,60 @@ var digit = [
         [0, 0, 0, 0]
     ] //:
 ];
+var WINDOW_WIDTH = 1024;
+var WINDOW_HEIGHT = 768;
+var RADIUS = 8;
+var MARGIN_TOP = 60;
+var MARGIN_LEFT = 30;
+
+//时间月份 12-1 代表12月，因为是从0开始的
+//注意，时间小时是2位数字，这里的时间最多能倒计时4天 4 * 24 ，如果时间太长会出错，需要扩充设置天的倒计时，年的倒计时，月的倒计时
+//结束时间
+const endTime = new Date(2016,12-1,5,18,28,0);
+//new Date(年,月,日,小时,分,秒);
+var curShowTimeSeconds = 0;
 window.onload = function() {
 
     var canvas = document.getElementById('canvas');
+    var context = canvas.getContext('2d');
+
     canvas.width = WINDOW_WIDTH;
     canvas.height = WINDOW_HEIGHT;
-    var context = canvas.getContext('2d');
-    render(context);
+
+    curShowTimeSeconds = getCurrentShowTimeSeconds();
+
+    setInterval(function(){
+        render(context);
+        update();
+    },50);
+
 };
 
-
+//启动绘制函数
 function render(cxt) {
-    var hours = 12;
-    var minutes = 36;
-    var seconds = 58;
-    renderDigit(0, 0, parseInt(seconds / 10), cxt);
+    //每次定时器的绘制需要重新刷新视图
+    cxt.clearRect(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
 
+    //定义时间
+    var hours = parseInt(curShowTimeSeconds / 3600);
+    var minutes = parseInt( (curShowTimeSeconds - hours * 3600) / 60);
+    var seconds = curShowTimeSeconds % 60;
+    //小时
+    renderDigit(MARGIN_LEFT, MARGIN_TOP, parseInt(hours / 10), cxt);
+    renderDigit(MARGIN_LEFT + 15 * (RADIUS + 1), MARGIN_TOP, parseInt(hours % 10), cxt);
+    // 冒号
+    renderDigit(MARGIN_LEFT + 30 * (RADIUS + 1), MARGIN_TOP, 10, cxt);
+    // 分
+    renderDigit(MARGIN_LEFT + 39 * (RADIUS + 1), MARGIN_TOP, parseInt(minutes / 10), cxt);
+    renderDigit(MARGIN_LEFT + 54 * (RADIUS + 1), MARGIN_TOP, parseInt(minutes % 10), cxt);
+    // 冒号
+    renderDigit(MARGIN_LEFT + 69 * (RADIUS + 1), MARGIN_TOP, 10, cxt);
+    // 秒
+    renderDigit(MARGIN_LEFT + 78 * (RADIUS + 1), MARGIN_TOP, parseInt(seconds / 10), cxt);
+    renderDigit(MARGIN_LEFT + 93 * (RADIUS + 1), MARGIN_TOP, parseInt(seconds % 10), cxt);
 }
 
+//执行绘制函数
 function renderDigit(x, y, num, cxt) {
     cxt.fillStyle = "rgb(0,102,153)";
 
@@ -160,9 +193,38 @@ function renderDigit(x, y, num, cxt) {
         for (var j = 0; j < digit[num][i].length; j++)
             if (digit[num][i][j] == 1) {
                 cxt.beginPath();
-                cxt.arc(x + j * 2 * (RADIUS + 1) + (RADIUS + 1), y + i * 2 * (RADIUS + 1) + (RADIUS + 1), RADIUS, 0, 2 * Math.PI)
+                cxt.arc(x + j * 2 * (RADIUS + 1) + (RADIUS + 1), y + i * 2 * (RADIUS + 1) + (RADIUS + 1), RADIUS, 0, 2 * Math.PI);
                 cxt.closePath();
 
                 cxt.fill();
             }
+}
+
+//获取倒计时时间
+function getCurrentShowTimeSeconds(){
+    var curTime = new Date();
+    var ret = endTime.getTime() - curTime.getTime();
+    ret = Math.round(ret/1000); //返回最接近的整数，四舍五入
+
+    return ret >=0 ? ret : 0;
+}
+
+//调整数据
+function update(){
+
+    //时间更新 ？感觉不需要这么复杂去比较值 ，可能为了动画更平滑吧
+    var nextShowTimeSeconds = getCurrentShowTimeSeconds();
+
+    var nextHours = parseInt(nextShowTimeSeconds / 3600);
+    var nextMinutes = parseInt( (nextShowTimeSeconds - nextHours * 3600) / 60);
+    var nextSeconds = nextShowTimeSeconds % 60;
+
+    var curHours = parseInt(curShowTimeSeconds / 3600);
+    var curMinutes = parseInt( (curShowTimeSeconds - curHours * 3600) / 60);
+    var curSeconds = curShowTimeSeconds % 60;
+
+    if (nextSeconds != curSeconds) {
+        curShowTimeSeconds = nextShowTimeSeconds;
+    }
+
 }
